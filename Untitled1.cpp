4ef3a32b4915d4df478d9fc6aba0c106d60224e2
kulsum358 <51861774+kulsum358@users.noreply.github.com>
2020-08-21 12:27:30 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,19 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a, p;
+    cin >>a >>p;
+    cout <<(p + (3*a)) / 2;
+    return 0;
+}
+
